a49d6918d46de67cdf407f203efccad41e41fc01
Gal Mitrani <gal.mitrani@cs.huji.ac.il>
2023-04-25 10:42:35 +0300

    movies categories links

---
+++ MoviesCategories.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+PATH = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+START_LINK = "https://www.imdb.com"
+
+
+def get_links_list_of_movies_categories():
+    links = []
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(PATH)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+
+    for bar in soup.find_all("div", {"class": "aux-content-widget-2"}):
+        # for category_bar in bar.find_all("span", {"class": "ab-widget"}):
+        for category_tag in bar.find_all('a'):
+            links.append(START_LINK + category_tag['href'])
+    return links
+
+
+if __name__ == "__main__":
+    print(get_links_list_of_movies_categories())
